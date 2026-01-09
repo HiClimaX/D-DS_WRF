18b2ea0ef15f9fe52616bfcca5c07d0eb1d4bdaf
Do Ngoc Khanh <14163153+epn09@users.noreply.github.com>
2026-01-09 12:06:15 +0900

    Fix empty array problem with old bash version

--- get_6h_urls.sh
+++ get_6h_urls.sh
@@ -23,14 +23,14 @@ member_id=r1i1p1f1
 
 awk -F, '(NR > 1) && $2' model_conf/"$source_id".csv |
 	parallel -j 4 --delay 3 --lb --colsep=, python get_6h_url.py \
-		"${options[@]}" --variable_id '{1}' --table_id '{2}' --frequency '{3}' \
+		"${options[@]+"${options[@]}"}" --variable_id '{1}' --table_id '{2}' --frequency '{3}' \
 		--source_id "$source_id" --member_id "$member_id" --experiment_id historical \
 		--start_date "2010-01-02" --end_date "2010-01-30" |
 	tee "$data_dir/urls.txt"
 
 awk -F, '(NR > 1) && $2' model_conf/"$source_id".csv |
 	parallel -j 4 --delay 3 --lb --colsep=, python get_6h_url.py \
-		"${options[@]}" --variable_id '{1}' --table_id '{2}' --frequency '{3}' \
+		"${options[@]+"${options[@]}"}" --variable_id '{1}' --table_id '{2}' --frequency '{3}' \
 		--source_id "$source_id" --member_id "$member_id" --experiment_id ssp370 \
 		--start_date "2050-01-02" --end_date "2050-01-30" |
 	tee -a "$data_dir/urls.txt"
